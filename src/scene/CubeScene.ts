496adca9bee7b09150c7baa0587b37fb8212881e
lebrancconvas <lebrancconvas@gmail.com>
2022-03-18 12:29:34 +0700

    ✨ Scene Drawing Class.

---
+++ src/scene/CubeScene.ts
@@ -0,0 +1,26 @@
+import * as THREE from 'three';
+
+class CubeScene extends THREE.Scene {
+	initialize() {
+		// Create Geometry and Material. 
+		const geometry = new THREE.BoxGeometry();
+		const material = new THREE.MeshPhongMaterial({color: 0xFFAA00});
+
+		// Create Cube Mesh. 
+		const cube = new THREE.Mesh(geometry, material);
+		cube.position.z = -5; // Assign Local Position (Z) of an abject. 
+		cube.position.y = 1; // Assign Local Position (Y) of an object. 
+
+		// Add Cube Mesh to Scene. 
+		this.add(cube);
+
+		// Create Light. 
+		const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+		light.position.set(0, 4, 2);
+
+		// Add Light to Scene. 
+		this.add(light);
+	}
+}
+
+export default CubeScene;
