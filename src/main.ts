380ce2bfaf589dca3a3e31ccb1de65d8d224a907
lebrancconvas <lebrancconvas@gmail.com>
2022-03-18 11:17:19 +0700

    💡 Comment on each line to explain the workflow.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import * as THREE from 'three';
 
+// Get Width and Height. 
 const width: number = window.innerWidth;
 const height: number = window.innerHeight;
 
+// Create Graphics Renderer. 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.getElementById('app') as HTMLCanvasElement
 });
 
+// Set Renderer's Area. 
 renderer.setSize(width, height);
 
+// Create Main Camera as a Perspective Camera. `
 const mainCamera = new THREE.PerspectiveCamera(60, width / height, 0.1, 100);
 
+// Create Scene. 
 const scene = new THREE.Scene;
 
+// Using Renderer to render the Scene and the Main Camera. 
 renderer.render(scene, mainCamera); 
