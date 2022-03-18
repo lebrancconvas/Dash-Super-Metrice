5b993468de30d6a956f2ac3ed9f1b7f9bb60d9bc
lebrancconvas <lebrancconvas@gmail.com>
2022-03-19 02:39:35 +0700

    ✨ Edit Main Scene.

--- src/scene/MainScene.ts
+++ src/scene/MainScene.ts
@@ -1,54 +1,47 @@
 import * as THREE from 'three';
 
-import {FBXLoader} from 'three/examples/jsm/loaders/FBXLoader';
+// import {FBXLoader} from 'three/examples/jsm/loaders/FBXLoader';
 import {MTLLoader} from 'three/examples/jsm/loaders/MTLLoader';
 import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader';
 
 const π = Math.PI;
 
-class CubeScene extends THREE.Scene {
+class MainScene extends THREE.Scene {
 
 	// Assign Loader Variable. 
-	private readonly fbxLoader = new FBXLoader();
+	// private readonly fbxLoader = new FBXLoader();
 	private readonly mtlLoader = new MTLLoader();
 	private readonly objLoader = new OBJLoader();
-
-	// Assign Assets's Path Variable. 
-	assetName = "block";
-
-	assetsBasePath = "../../public/assets/";
-	assetsType = "3d/";
-
-	assetsMTLTarget = `Platformer_Kit/Models/OBJ format/${this.assetName}.mtl`;
-	assetsMTLPath = `${this.assetsBasePath}${this.assetsType}${this.assetsMTLTarget}`;
-	
-	assetsOBJTarget = `Platformer_Kit/Models/OBJ format/${this.assetName}.obj`;
-	assetsOBJPath = `${this.assetsBasePath}${this.assetsType}${this.assetsOBJTarget}`;
 	
 	// Scene Method. 
 	async initialize() {
-		const objectMTL = await this.mtlLoader.loadAsync(this.assetsMTLPath);
-		objectMTL.preload();
-
-		const block1 = await this.createObject(objectMTL);
-		const block2 = await this.createObject(objectMTL);
+		const block1 = await this.loadObject('Platformer_Kit', 'block');
+		const block2 = await this.loadObject('Platformer_Kit', 'block');
+		const bridge = await this.loadObject('Platformer_Kit', 'bridge');
+		const car = await this.loadObject('Car_kit', 'race');
 
 		this.translateObject(block1, -1, -0.8, -5);
 		this.translateObject(block2, 1, -0.8, -5);
+		this.translateObject(bridge, 0, -0.5, -5);
+		this.translateObject(car, 0, -1, -5);
 
-		this.add(block1, block2);
+		this.add(block1, block2, bridge, car);
 
 		this.createLight();
 	}
 
-	private async createObject(mtl: MTLLoader.MaterialCreator) {
-		this.objLoader.setMaterials(mtl);
+	private async loadObject(objectSet: String, objectName: String) {
+		const mtlPath = `assets/3d/${objectSet}/Models/OBJ format/${objectName}.mtl`;
+		const objPath = `assets/3d/${objectSet}/Models/OBJ format/${objectName}.obj`;
+		const MTL = await this.mtlLoader.loadAsync(mtlPath);
+		MTL.preload();
+		this.objLoader.setMaterials(MTL);
 
-		const model = await this.objLoader.loadAsync(this.assetsOBJPath);
-		model.rotateX(this.degreeToRadian(20));
+		const model = await this.objLoader.loadAsync(objPath);
+		model.rotateX(this.degreeToRadian(16));
 
 		return model;
-	} 
+	}
 
 	private async translateObject(object: THREE.Group, xAxis: number, yAxis: number, zAxis: number) {
 		object.position.x = xAxis;
@@ -76,11 +69,11 @@ class CubeScene extends THREE.Scene {
 	createLight() {
 			// Create Light. 
 			const light = new THREE.DirectionalLight(0xFFFFFF, 1);
-			light.position.set(0, 4, 5);
+			light.position.set(0, 3, 5);
 	
 			// Add Light to Scene. 
 			this.add(light);
 	}
 }
 
-export default CubeScene;
+export default MainScene;
